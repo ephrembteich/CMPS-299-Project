532db225ea89325254b867b629d59a6e8f2290bf
iah10 <issahaddar5@gmail.com>
2016-03-10 19:22:51 +0200

    First Commit

---
+++ Assets/introFunction.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class introFunction : MonoBehaviour {
+
+	private int age = 0;
+	private int gender = 0;
+	public GameObject nextBtn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void setAge(int param){
+		age = param;
+		if(gender!=0){
+			nextBtn.GetComponentInChildren<Button> ().interactable = true;
+		}
+	}
+
+	public void setGender(int param){
+		gender = param;
+		if(age!=0){
+			nextBtn.GetComponentInChildren<Button> ().interactable = true;
+		}
+	}
+
+	public void next(){
+		Application.LoadLevel("randomLevel");
+	}
+}
---
+++ Assets/randomScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void randomScenario(){
+		//in this method you random between all the scenarios
+		//and you choose one of them and load it
+		//use this for loading the scene Application.LoadLevel();
+	}
+}
