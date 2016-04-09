920823283a065c6bc80c63f93b5300554c0d8f25
Ephrem Bteich <ehb03@mail.aub.edu>
2016-04-09 15:44:41 +0300

    change gender variable type to string

--- Assets/introFunction.cs
+++ Assets/introFunction.cs
@@ -6,7 +6,7 @@ using System;
 public class introFunction : MonoBehaviour {
 
 	private int age = 0;
-	private int gender = 0;
+	private string gender = null;
 	public GameObject nextBtn;
 
 	// Use this for initialization
@@ -21,12 +21,12 @@ public class introFunction : MonoBehaviour {
 
 	public void setAge(int param){
 		age = param;
-		if(gender!=0){
+		if(gender!=null){
 			nextBtn.GetComponentInChildren<Button> ().interactable = true;
 		}
 	}
 
-	public void setGender(int param){
+	public void setGender(string param){
 		gender = param;
 		if(age!=0){
 			nextBtn.GetComponentInChildren<Button> ().interactable = true;
